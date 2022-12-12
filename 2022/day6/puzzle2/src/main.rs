7caa0ef8d71eee7820e379740c080233d0bbfb5f
Ellie-exe <ryiscool247@gmail.com>
2022-12-12 17:27:25 -0500

    Day 6 puzzle 2 solution

---
+++ 2022/day6/puzzle2/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn main() {
+    let buffer = fs::read_to_string("input.txt").unwrap();
+    let mut previous = String::new();
+    let mut marker = 0;
+
+    for char in buffer.chars() {
+        marker += 1;
+
+        if let Some(i) = previous.find(char) {
+            previous = previous[i + 1..].to_string() + &char.to_string();
+            continue;
+        }
+
+        previous.push(char);
+        if previous.len() == 14 { break; }
+    }
+
+    println!("The first marker is after character: {}", marker);
+}
